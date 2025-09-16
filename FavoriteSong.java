bc3ee93310efd85ca599bce3aa15e09189d66b8d
JaredPaulIlustrisimo-UCM <jaredilustrisimo3@gmail.com>
2025-09-16 20:58:32 +0800

    Add files via upload

---
+++ FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("I should have stayed at home,");
+        System.out.println("'Cause right now I see all these people that love me but I still feel alone");
+        System.out.println("Can't help but check my phone");
+        System.out.println("I could have made you mine, but no it wasn't meant to be");
+    }
+}
