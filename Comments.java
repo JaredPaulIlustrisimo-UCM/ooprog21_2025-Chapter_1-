0f097ca50d9c46156e7b714e6a03f3d2df92f362
JaredPaulIlustrisimo-UCM <jaredilustrisimo3@gmail.com>
2025-09-16 21:16:10 +0800

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,20 @@
+/**
+ * This is a documentation comment.
+ * It is used to describe the class, method, or variable
+ * and can be extracted to generate external documentation.
+ */
+public class Comments {
+
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+        System.out.println("Java comment types");
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         * Useful for temporarily disabling code or adding explanations.
+         */
+
+    }
+}
